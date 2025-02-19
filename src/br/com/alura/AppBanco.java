37b9073f310c8fa1c76da896a2d09a8b3d42f40c
fernandopereira <fernandoa.pereira@telefonica.com>
2025-02-18 21:48:59 -0300

    executando operacoes

---
+++ src/br/com/alura/AppBanco.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+import java.math.BigDecimal;
+
+public class AppBanco {
+
+    public static void main(String[] args){
+        var cliente = new Cliente("João");
+        var conta = new Conta(cliente,new BigDecimal("150"));
+
+        var operacao = new OperacaoSaque(conta,new BigDecimal("150"));
+
+        operacao.executa();
+        operacao.executa();
+
+    }
+
+}
